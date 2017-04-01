6ae281df3ef61499a22f5d7d45eee8bd9c0add24
Brett East <brett@sproutback.com>
2017-04-01 12:22:22 +1100

    Complete handleAddTodo

--- app/tests/components/ToDoApp.test.jsx
+++ app/tests/components/ToDoApp.test.jsx
@@ -10,4 +10,14 @@ describe('ToDoApp', () => {
   it('should exist', () => {
       expect(ToDoApp).toExist();
     });
+
+  it('should add todo to the todos state onHandleAddTodo', () => {
+    var todoText = 'test test';
+    var todoApp = TestUtils.renderIntoDocument(<ToDoApp/>);
+
+    todoApp.setState({todos:[]});
+    todoApp.handleAddTodo(todoText);
+
+    expect(todoApp.state.todos[0].text).toBe(todoText);
+  });
 });
